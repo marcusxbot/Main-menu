314429017426d029e56088260464d24cf969893b
Marcus Salér <marcus.saler@elev.ga.lbs.se>
2019-01-10 10:56:40 +0100

    Allt på Torsdag

---
+++ Assets/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Rotate : MonoBehaviour
+{
+
+
+    public void Rotator(float degrees)
+    {
+        transform.Rotate(0, 0, degrees);
+    }
+}
